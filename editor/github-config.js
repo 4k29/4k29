46e5a947adda0bf1ff8a1974293913b25729bd36
4k29 <2hjpkx4564@privaterelay.appleid.com>
2026-07-26 09:56:06 +0900

    Configure direct publishing repository

--- editor/github-config.js
+++ editor/github-config.js
@@ -2,5 +2,7 @@ window.EDITOR_GITHUB_CONFIG = Object.freeze({
   owner: "4k29",
   repository: "4k29-editor-drafts",
   branch: "main",
+  publicRepository: "4k29",
+  publicBranch: "main",
   setupUrl: "/4k29/editor/setup/"
 });
